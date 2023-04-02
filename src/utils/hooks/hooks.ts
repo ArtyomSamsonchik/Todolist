34f6448a6e7492d60397a89ee1251efa525b68f4
ArtyomSamsonchik <colpachek@gmail.com>
2023-04-03 02:52:26 +0300

    feat(hooks): add useWithLoadingState hook to track loading in components

--- src/utils/hooks/hooks.ts
+++ src/utils/hooks/hooks.ts
@@ -1,5 +1,24 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from '../../app/store'
+import { useState } from 'react'
 
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export const useWithLoadingState = (initIsLoading = false) => {
+  const [isLoading, setIsLoading] = useState(initIsLoading)
+
+  type CallBack<T extends any[], R> = (...args: T) => R | Promise<R>
+
+  const resolveCallback = <TParams extends any[], TResult>(cb: CallBack<TParams, TResult>) => {
+    return async (...args: TParams) => {
+      setIsLoading(true)
+      const result = await cb(...args)
+      setIsLoading(false)
+
+      return result
+    }
+  }
+
+  return [isLoading, resolveCallback] as const
+}
